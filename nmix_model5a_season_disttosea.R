e422213fa74a93946bf332834f1a2597a401d2a1
schultz7676 <49698947+schultz7676@users.noreply.github.com>
2019-06-01 17:33:16 -0700

    Add files via upload

---
+++ nmix_model5a_season_disttosea.R
@@ -0,0 +1,60 @@
+source("./From Madsen/gen.nmix.R")
+library(readr)
+library("unmarked")
+library("tictoc") #So we can see our code's run time
+library(beepr)
+
+source("./nmix_model_setup.R")
+
+Method = "BFGS"
+K.lim = max(ceiling(as.matrix(n.it)/100),na.rm=TRUE)+20
+
+#Closed model, binned responses ####
+start.vals = c(0,0)
+y = unmarkedFramePCount(ceiling(as.matrix(n.it)/100),
+                        siteCovs=as.data.frame(X),
+                        obsCovs=as.data.frame(Z),
+                        mapInfo=NULL)
+model.closed = pcount(~1 ~1,
+                      y,
+                      K.lim,
+                      mixture="P",
+                      start.vals,
+                      method=Method,
+                      se=TRUE,
+                      engine="C")
+
+#Open Model covariates, binned responses ####
+y = unmarkedFramePCO(ceiling(as.matrix(n.it)/100),
+                     siteCovs=as.data.frame(X),
+                     obsCovs=as.data.frame(Z),
+                     yearlySiteCovs=NULL,
+                     mapInfo=NULL,
+                     numPrimary=33,
+                     primaryPeriod=DATE.4)
+
+tic()
+model.open.covariate = pcountOpen(~d.to.sea,~1,~1,~season,
+                                  y,
+                                  mixture="P",
+                                  K.lim,
+                                  dynamics="constant",
+                                  fix="none",
+                                  starts=c(model.closed@opt$par[1],rep(0,5),model.closed@opt$par[2]),
+                                  method=Method,
+                                  se=TRUE,
+                                  immigration=FALSE,
+                                  iotaformula=~1)
+toc() # 695.178 sec on Jacob's machine
+beep()
+summary(model.open.covariate)
+
+lam <- 100*exp(coef(model.open.covariate, type="lambda"))
+gam <- exp(coef(model.open.covariate, type="gamma"))
+om <- plogis(coef(model.open.covariate, type="omega"))
+p <- plogis(coef(model.open.covariate, type="det"))
+c(lam,gam,om,p) #back transformed fitted values
+
+ev.open = eigen(model.open.covariate@opt$hessian)$values
+cn.open = max(ev.open)/min(ev.open)
+cn.open
